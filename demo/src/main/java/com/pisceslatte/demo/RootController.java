23c1708ee1547d4872c9ec18e22b09ac82d7df88
PiscesLatte <st81370307@gmail.com>
2022-04-16 16:31:23 +0800

    pass parameter to html

--- demo/src/main/java/com/pisceslatte/demo/RootController.java
+++ demo/src/main/java/com/pisceslatte/demo/RootController.java
@@ -1,13 +1,15 @@
 package com.pisceslatte.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message","Hi, I'm PiscesLatte");
         return "home";
     }
 }
