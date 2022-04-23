18598065d152768e2fad7c1e205bb6ba9519af73
PiscesLatte <st81370307@gmail.com>
2022-04-23 10:23:41 +0800

    新增MockMvc的單元測試

--- demo/src/test/java/com/pisceslatte/demo/Test2.java
+++ demo/src/test/java/com/pisceslatte/demo/Test2.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,5 +21,17 @@ public class Test2 {
     @Test
     public void testNotNull(){
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Work for")));
+    }
+
+    @Test
+    public void test404() throws Exception {
+        mockMvc.perform(get("/pisceslatte")).andExpect(status().isNotFound());
     }
 }
