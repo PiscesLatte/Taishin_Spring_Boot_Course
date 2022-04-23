f2b03ac4ddec7c987f78d02e44309d37568716c0
PiscesLatte <st81370307@gmail.com>
2022-04-23 14:14:03 +0800

    Add beans

---
+++ demo/src/main/java/com/pisceslatte/demo/beans/User3.java
@@ -0,0 +1,15 @@
+package com.uuu.demo1.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User3 {
+    @NotBlank(message="name should not be blank")
+    private String name;
+}
